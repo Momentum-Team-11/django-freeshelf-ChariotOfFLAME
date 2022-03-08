787bd2af5cc169176de2f6df927970e9b4d4203a
Adam Lindgren <adam@adamlindgren.com>
2022-03-08 10:21:40 -0500

    completed registration

--- users/models.py
+++ users/models.py
@@ -1,12 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     def __repr__(self):
         return f"<User username={self.username}>"
 
     def __str__(self):
         return self.username
-
-
